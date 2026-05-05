d17f43d474192544a47f55071377c8d218cb564a
manuelhgm <manuelmesquita.aluno@unipampa.edu.br>
2026-05-04 21:46:11 -0300

    inicio do datapath e adição dos registradores na struct geral

--- testes/datapath/datapath.c
+++ testes/datapath/datapath.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h>
+#include"dcl_str_p2.h"
 
 int main() // usado para fins de testes somente
 {
 
 }
-
-int caminho_de_dados()
+uint8_t mux(uint8_t A,uint8_t B,bool sinal);
+int caminho_de_dados(typ_state **c, bool clock, bool clear)
 {
-    
+    // geração do sinal com or entre PCesc e o resuldado da and brench e zero ula
+    // escrita no PC
+
+    mux((**c).registrador.PC, (**c).registrador.intermediario.ULA_saida, (**c).sinais[IouD]);
+    //acesso a memoria memoria()
+
+    // escrita nos intermediarios RD e MDR
+
+    //controle
+    //banco de registradores
+    //
+}
+
+uint8_t mux(uint8_t A,uint8_t B,bool sinal)
+{
+    if (sinal == 0)
+        return A;
+    return B;
 }
--- testes/datapath/dcl_str_p2.h
+++ testes/datapath/dcl_str_p2.h
@@ -1,15 +1,36 @@
 #include<stdbool.h>
+#include<stdint.h>
 
 #ifndef dcl_str_p2_H
 #define dcl_str_p2_H
 
+    // declaração das strings projeto 2
+
+    typedef struct 
+    {
+        uint16_t RI;
+        uint16_t RDM;
+        uint8_t A;
+        uint8_t B;
+        uint8_t ULA_saida;
+    }tpy_int_reg;
+
+    typedef struct 
+    {
+        uint8_t banco[8];
+        tpy_int_reg intermediario;
+        uint8_t PC;
+    }typ_all_reg;
+    
+    
     typedef struct 
     {
         
+        typ_all_reg registrador;
         bool sinais[20];
     }typ_state;
 
-    enum // sinais
+    enum // sinais do controle
     {
         ControleUla1 =  0,
         ControleUla2 =  1,
@@ -30,7 +51,7 @@
         estado0 = 16,
         estado1 = 17,
         estado2 = 18,
-        estado3 = 19
+        estado3 = 19,
     };
     
 

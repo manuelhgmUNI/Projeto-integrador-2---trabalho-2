09c2275b7d2e00aa9dfeb38f751add378f8afcdf
renanmartinsaluno-ui <renanmartins.aluno@unipampa.edu.br>
2026-05-15 11:43:27 -0300

    Refactor controle_ula_fonte function for clarity

--- controleUlaFonte.c
+++ controleUlaFonte.c
@@ -2,55 +2,85 @@
 
 void controle_ula_fonte(typ_state **c)
 {
-    // limpa sinais
-    (**c).sinais[UlaFonteA] = 0;
-    (**c).sinais[UlaFonteB1] = 0;
-    (**c).sinais[UlaFonteB0] = 0;
-
     bool e0 = (**c).sinais[estado0];
     bool e1 = (**c).sinais[estado1];
     bool e2 = (**c).sinais[estado2];
     bool e3 = (**c).sinais[estado3];
+    
+    // RESET DOS SINAIS
+    (**c).sinais[UlaFonteA] = 0;
 
-    //FETCH PC = PC + 1
+    (**c).sinais[UlaFonteB0] = 0;
+    (**c).sinais[UlaFonteB1] = 0;
+
+    (**c).sinais[ControleUla1] = 0;
+    (**c).sinais[ControleUla2] = 0;
+    (**c).sinais[ControleUla3] = 0;
+
+    // FETCH ESTADO 0000 ULA = PC + 1
 
     if ((!e3 && !e2) && (!e1 && !e0))
     {
+        //Entrada A = PC
         (**c).sinais[UlaFonteA] = 0;
 
+        //Entrada B = 1
         (**c).sinais[UlaFonteB1] = 0;
         (**c).sinais[UlaFonteB0] = 1;
+
+        // Operação = ADD ALUOp = 000
+        (**c).sinais[ControleUla1] = 0;
+        (**c).sinais[ControleUla2] = 0;
+        (**c).sinais[ControleUla3] = 0;
     }
 
-    // Tipo R A op B
-    
+    // EXECUÇÃO TIPO R
     if ((**c).instrucao.tipo == r)
     {
+        // A = registrador A
         (**c).sinais[UlaFonteA] = 1;
 
+        // B = registrador B
         (**c).sinais[UlaFonteB1] = 0;
         (**c).sinais[UlaFonteB0] = 0;
+
+        //  ALUOp = 010  usa funct
+        (**c).sinais[ControleUla1] = 0;
+        (**c).sinais[ControleUla2] = 1;
+        (**c).sinais[ControleUla3] = 0;
     }
 
-    // LW / SW / ADDI  base + imediato
-
+    // LW / SW / ADDI
     if ((**c).instrucao.opcode == 11 || // lw
         (**c).instrucao.opcode == 15 || // sw
         (**c).instrucao.opcode == 4)    // addi
     {
+        // A = registrador base
         (**c).sinais[UlaFonteA] = 1;
 
+        // B = imediato
         (**c).sinais[UlaFonteB1] = 1;
         (**c).sinais[UlaFonteB0] = 0;
+
+        //  ALUOp = 000 ADD
+        (**c).sinais[ControleUla1] = 0;
+        (**c).sinais[ControleUla2] = 0;
+        (**c).sinais[ControleUla3] = 0;
     }
 
-    // BEQA - B
-
+    //BEQ
     if ((**c).instrucao.opcode == 8)
     {
+        // A = registrador A
         (**c).sinais[UlaFonteA] = 1;
 
+        // B = registrador B
         (**c).sinais[UlaFonteB1] = 0;
         (**c).sinais[UlaFonteB0] = 0;
+
+        // ALUOp = 001 SUB
+        (**c).sinais[ControleUla1] = 0;
+        (**c).sinais[ControleUla2] = 0;
+        (**c).sinais[ControleUla3] = 1;
     }
 }
